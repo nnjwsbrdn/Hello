02d9924e0f333f29dd816e2768bd1706632763fe
nnjwsbrdn <116859559+nnjwsbrdn@users.noreply.github.com>
2022-11-25 10:43:50 +0800

    Add files via upload

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            int cnt;
+            for(cnt =5; cnt <=25; cnt+=5)
+            {
+                Console.Write(cnt + " ");
+            }
+            Console.WriteLine();
+
+            int year = 2022;
+            Console.WriteLine(year+ " ");
+            if ((year % 400) == 0)
+            {
+                Console.WriteLine("Leap Year");
+            }
+            else if ((year % 100) == 0)
+            {
+                Console.WriteLine("Not Leap Year");
+            }
+            else if ((year % 4) == 0)
+            {
+                Console.WriteLine("Not Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Not Leap Year");
+            }
+            }
+
+        }
+        
+}
+
